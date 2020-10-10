4fff8e1ed0303f2fe16e258d0f4728a6fb4b2cc1
TheReallyWOLF <wolf-04-654@yandex.ru>
2020-10-10 22:12:26 +0300

    car

--- firstStepAngularTask/src/app/components/car/car.component.ts
+++ firstStepAngularTask/src/app/components/car/car.component.ts
@@ -7,9 +7,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarComponent implements OnInit {
 
+  name: string;
+  speed: number;
+  model: string;
+  colors: Colors;
+  options: string[];
+  test: any;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.name = 'Audi';
+    this.speed = 235;
+    this.model = 'RS8';
+    this.speed = 235;
+    this.colors = {
+      car: 'White',
+      salon: 'Black',
+      wheels: 'Silver'
+    };
+    this.options = ['ABS', 'Автопилот', 'Авто паркинг'];
+    this.test = true;
   }
+    carSelect(carName) {
+    if(carName == 'bmw'){
+      this.name = 'BMW';
+      this.speed = 280;
+      this.model = 'M5';
+      this.colors = {
+        car: 'Синий',
+        salon: 'Жёлтый',
+        wheels: 'Красный'
+      };
+      this.options = ['ABS', 'Авто паркинг'];
+      this.test = true;
+    }else if(carName == 'audi'){
+      this.name = 'Audi';
+      this.speed = 235;
+      this.model = 'RS8';
+      this.speed = 235;
+      this.colors = {
+        car: 'White',
+        salon: 'Black',
+        wheels: 'Silver'
+      };
+      this.options = ['ABS', 'Автопилот', 'Авто паркинг'];
+      this.test = true;
+    }else if(carName == 'mer'){
+      this.name = 'Mercedes';
+      this.speed = 190;
+      this.model = 'C180';
+      this.speed = 205;
+      this.colors = {
+        car: 'Black',
+        salon: 'Black',
+        wheels: 'Black'
+      };
+      this.options = ['Автопилот', 'Авто паркинг'];
+      this.test = true;
+    }
 
+    };
+}
+
+interface Colors {
+  car: string;
+  salon: string;
+  wheels: string;
 }
