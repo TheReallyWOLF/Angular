d338afc9158eadebb93bbafda7aea0ba483c5363
TheReallyWOLF <wolf-04-654@yandex.ru>
2020-10-11 17:22:42 +0300

    router

--- firstStepAngularTask/src/app/app.module.ts
+++ firstStepAngularTask/src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms";
+import { RouterModule, Routes } from "@angular/router"; // маршрутизатор
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarComponent } from './components/car/car.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
 
+// маршруты для переключения страниц
+const appRoutes: Routes = [
+  {path: '', component:CarComponent},
+  {path: 'about', component:ContactsComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
-    CarComponent
+    CarComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes) // управляющий объект для роутинга
   ],
   providers: [],
   bootstrap: [AppComponent]
