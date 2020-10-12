7aa1afa82f56cca9d3464bd90938a80d972ead9d
TheReallyWOLF <wolf-04-654@yandex.ru>
2020-10-12 17:16:58 +0300

    new

---
+++ organizer/src/app/shared/date.service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from "@angular/core";
+import * as moment from "moment";
+import {BehaviorSubject} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class DateService {
+  public date: BehaviorSubject<moment.Moment> = new BehaviorSubject(moment())
+
+  changeMounth(dir: number){
+    const value = this.date.value.add(dir, 'month');
+    this.date.next(value);
+  }
+}
