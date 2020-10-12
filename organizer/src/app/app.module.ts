34df42c0367113fa69454684d4cbbadd7c4f271c
TheReallyWOLF <wolf-04-654@yandex.ru>
2020-10-12 17:17:17 +0300

    new

---
+++ organizer/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { SelectorComponent } from './selector/selector.component';
+import { OrganizerComponent } from './organizer/organizer.component';
+import { MomentPipe } from "./shared/moment.pipe";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CalendarComponent,
+    SelectorComponent,
+    OrganizerComponent,
+    MomentPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
