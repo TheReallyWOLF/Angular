3b5e29b3bdaa88b6b61308b0613bcecfeeaea304
TheReallyWOLF <wolf-04-654@yandex.ru>
2020-10-13 18:55:02 +0300

    3

--- angular-basics/src/app/card/card.component.ts
+++ angular-basics/src/app/card/card.component.ts
@@ -4,6 +4,25 @@ import {Component} from "@angular/core";
 @Component({
   selector: 'app-card',
   templateUrl:'./card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
+  //interpolation:['изменитьИнтерполяцию','изменитьИнтерполяцию'] поменять {{}}
 })
-export class CardComponent {}
+export class CardComponent {
+
+  title = 'My Card Title';
+  text = 'My text';
+  number = 777;
+  array = [1,2,3,4,5,12,2312,3,21];
+  obj = {
+    name: 'Vaysa',
+    age: 32,
+    info: {
+      status: 'alive',
+      skills: null
+    }
+  };
+
+  getInfo(){
+    return 'This is my info'; // если строчка возвращает строку ееё можно вызвать используя интерполяцию
+  }
+}
