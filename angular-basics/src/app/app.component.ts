9b9a717b6e6b5a47ad9d0dfa7fb451e6731a28dc
TheReallyWOLF <wolf-04-654@yandex.ru>
2020-10-14 12:11:44 +0300

    base

--- angular-basics/src/app/app.component.ts
+++ angular-basics/src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  toggle: boolean = true;
+
+  toggleCards(){
+    this.toggle = !this.toggle;
+  }
 }
