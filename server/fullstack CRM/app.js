0892ce6d8c3406d3c764b1934d4fcfc3cf9e5100
TheReallyWOLF <wolf-04-654@yandex.ru>
2020-10-19 16:44:35 +0300

    server

--- server/fullstack CRM/app.js
+++ server/fullstack CRM/app.js
@@ -7,6 +7,8 @@ const orderRoutes = require('./routes/order');
 const positionRoutes = require('./routes/position');
 const bodyParser = require('body-parser'); // библионтека которая парсит все данные приходящие от ползователя
 
+app.use(require('cors')());   // пакет для того точ бы сервер мог обрабатывать cors запросы
+app.use(require('morgan')('dev'));  // пакет для красивого отображдения что происходи на сервере в данный момент
 app.use(bodyParser.urlencoded({extended: true})); // кодирует URL
 app.use(bodyParser.json()); // Генерирует JS объекты из джейсона
 
