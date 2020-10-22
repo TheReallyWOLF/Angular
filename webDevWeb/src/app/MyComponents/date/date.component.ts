af5ad5ca3e451e08a82361e2920f700e87744e6b
TheReallyWOLF <wolf-04-654@yandex.ru>
2020-10-22 13:58:31 +0300

    WDB

--- webDevWeb/src/app/MyComponents/date/date.component.ts
+++ webDevWeb/src/app/MyComponents/date/date.component.ts
@@ -5,11 +5,46 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './date.component.html',
   styleUrls: ['./date.component.css']
 })
+
 export class DateComponent implements OnInit {
+  message: string = "World";
+  punctuationMark: string = "?";
+  DataTime: string = new Date().toLocaleTimeString('ru');
+  date: string = new Date().toDateString();
+  loggedIn: boolean = true;
+  foo = function (){
+    return "Выполнение ф-ии в интерполяции";
+  };
 
-  constructor() { }
+// Чаще делается так
+  answer: string;
+  text: string;
+  dataTimeInterval: string;
+  isCollapsed: boolean;
+  isCollapsedTwo: boolean;
 
-  ngOnInit(): void {
+
+  items = ["Angular", "React", "Vue", "Bootstrap", "Node.js"];
+
+  constructor() {
+    this.isCollapsedTwo = true;
+    this.isCollapsed = true;
+    this.answer = "Ответ";
+    this.text = "рыбий текст";
+    setInterval(() => {
+      this.dataTimeInterval = new Date().toLocaleTimeString('ru');
+    }, 1000);
+  };
+  myClickFoo(event){
+    console.log(event);
+  };
+  toogleCollapse(who){
+    if(who === 'one'){
+      this.isCollapsed = !this.isCollapsed;
+    }else if(who === 'two'){
+      this.isCollapsedTwo = !this.isCollapsedTwo;
+    }
+
   }
-
+  ngOnInit(): void {}
 }
