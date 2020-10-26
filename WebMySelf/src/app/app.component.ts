705d61c0d0c2b84bde52cff21371ff8065c098ce
TheReallyWOLF <wolf-04-654@yandex.ru>
2020-10-26 23:58:45 +0300

    New

--- WebMySelf/src/app/app.component.ts
+++ WebMySelf/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserService } from "./user.service";
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'WebMySelf';
+
+  users = [];
+
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit(){
+    //this.users = this.userService.users
+    this.userService.getUsers().subscribe(users => {
+      console.log(users)
+      this.users = users.results.map(item => ({
+        name: `${item.name.title} ${item.name.first} ${item.name.last}`,
+        image: item.picture.large,
+        geo: `${item.location.city} ${item.location.country} ${item.location.postcode} ${item.location.state}`
+      }))
+    })
+  }
 }
--- WebMySelf/src/app/app.module.ts
+++ WebMySelf/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponentComponent } from './user-component/user-component.component';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
