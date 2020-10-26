544cf35545bb1b394d8a8e5badc9386cfa1801d0
TheReallyWOLF <wolf-04-654@yandex.ru>
2020-10-27 00:00:37 +0300

    New

--- WebMySelf/src/app/app.component.ts
+++ WebMySelf/src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent {
     //this.users = this.userService.users
     this.userService.getUsers().subscribe(users => {
       console.log(users)
-      this.users = users.results.map(item => ({
+      this.users = users['results'].map(item => ({
         name: `${item.name.title} ${item.name.first} ${item.name.last}`,
         image: item.picture.large,
         geo: `${item.location.city} ${item.location.country} ${item.location.postcode} ${item.location.state}`
