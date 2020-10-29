7246840d28ce21dfe3ec6c86d5ebe03ee484ae87
TheReallyWOLF <wolf-04-654@yandex.ru>
2020-10-29 21:59:32 +0300

    work

--- curse/FirstApp/src/app/servers/servers.component.ts
+++ curse/FirstApp/src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer: boolean = false;
   serverCreationStatus: string = 'No server was created!';
+  serverName: string = '';
 
   constructor() {
     setTimeout(() => {
@@ -21,6 +22,10 @@ export class ServersComponent implements OnInit {
 
   onCreateServer(){
     this.serverCreationStatus = 'Server was created!';
+  };
+  onUpdateServerName(event: Event){
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
