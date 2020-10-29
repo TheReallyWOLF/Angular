5cea229fb04a24c5e40b3cb6a6fa681573c032ce
TheReallyWOLF <wolf-04-654@yandex.ru>
2020-10-29 21:36:45 +0300

    work

--- curse/FirstApp/src/app/servers/servers.component.ts
+++ curse/FirstApp/src/app/servers/servers.component.ts
@@ -7,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer: boolean = false;
+  serverCreationStatus: string = 'No server was created!';
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000)
+  }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created!';
   }
 
 }
