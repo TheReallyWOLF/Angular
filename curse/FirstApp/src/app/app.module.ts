072ce710158a101758b00fc0f2074c7fa803c12e
TheReallyWOLF <wolf-04-654@yandex.ru>
2020-10-29 17:05:06 +0300

    work

--- curse/FirstApp/src/app/app.module.ts
+++ curse/FirstApp/src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms";
-
 import { AppRoutingModule } from './app-routing.module';
+// components
 import { AppComponent } from './app.component';
+import { ServerComponent } from "./server/server.component";
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
