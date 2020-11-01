60c61a76941f9d83bd1dd5928de879f0757c976d
TheReallyWOLF <wolf-04-654@yandex.ru>
2020-11-01 17:50:46 +0300

    w

--- course/FirstApp/src/app/app.component.ts
+++ course/FirstApp/src/app/app.component.ts
@@ -8,6 +8,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   name:string = 'Wolf!';
   userName:string = '';
+  showSecret: boolean = false;
+  log: Array<any> = [];
+
+  onToggleDetails(){
+    this.showSecret = !this.showSecret;
+    //this.log.push(this.log.length+1);
+    this.log.push(new Date());
+  }
 
   resetUser(){
     this.userName = '';
