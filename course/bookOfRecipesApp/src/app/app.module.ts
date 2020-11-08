f9bfa50411bb36aaea61e7659f290e1424f1b63d
TheReallyWOLF <wolf-04-654@yandex.ru>
2020-11-08 16:47:18 +0300

    jquery

--- course/bookOfRecipesApp/src/app/app.module.ts
+++ course/bookOfRecipesApp/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from "./header/header.component";
+import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
 import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
--- course/bookOfRecipesApp/src/app/shopping-list/shopping-list.component.ts
+++ course/bookOfRecipesApp/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IngredientModel } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: IngredientModel[] = [
+    new IngredientModel('Apples', 5),
+    new IngredientModel('Tomatoes', 10)
+  ];
 
   constructor() { }
 
