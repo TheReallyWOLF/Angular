6d84c26c01eb88b53f403e3bec6cd65f7a32d542
TheReallyWOLF <wolf-04-654@yandex.ru>
2020-11-11 15:26:09 +0300

    rxjs

--- course/inputOutputProperties/src/app/cockpit/cockpit.component.ts
+++ course/inputOutputProperties/src/app/cockpit/cockpit.component.ts
@@ -1,32 +1,42 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component,
+  ElementRef,
+  EventEmitter,
+  OnInit, Output,
+  ViewChild, OnChanges,
+  SimpleChanges
+} from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
   templateUrl: './cockpit.component.html',
   styleUrls: ['./cockpit.component.css']
 })
-export class CockpitComponent implements OnInit {
+export class CockpitComponent implements OnInit, OnChanges {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>(); // вывод в родителя
   @Output('bpCreated') bluePrintCrated = new EventEmitter<{serverName: string, serverContent: string}>(); // вывод в родителя
-  newServerName = '';
-  newServerContent = '';
+  //newServerName = '';
+  //newServerContent = '';
+  @ViewChild('serverContentInput') serverContentInput: ElementRef; // добратся до значения через ссылку на элемент
 
   constructor() { }
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(changes)
+  }
 
   ngOnInit(): void {
   }
 
-  onAddServer() {
+  onAddServer(nameInput: HTMLInputElement) { // ссылка на инпут nameInput по ID
    this.serverCreated.emit({
-     serverName: this.newServerName,
-     serverContent: this.newServerContent
+     serverName: nameInput.value,
+     serverContent: this.serverContentInput.nativeElement.value
    });
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(nameInput: HTMLInputElement) {
     this.bluePrintCrated.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
