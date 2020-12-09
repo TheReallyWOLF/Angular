8485eaa3d2f9f3906995319a67660173292db2c6
TheReallyWOLF <wolf-04-654@yandex.ru>
2020-12-09 22:44:29 +0300

    recipes 90

--- course/bookOfRecipesApp/src/app/shopping-list/shopping-list.component.ts
+++ course/bookOfRecipesApp/src/app/shopping-list/shopping-list.component.ts
@@ -17,4 +17,8 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onIngredientAdded(ingredient: IngredientModel) {
+    this.ingredients.push(ingredient);
+  }
+
 }
