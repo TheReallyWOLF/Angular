0a51602cb44fea494018b05148005dbab1741225
Иванов Юрий Александрович <wolf-04-654@yandex.ru>
2021-10-30 13:36:56 +0300

    advanced angular

--- angularAdvanced/angular-advanced/src/app/app-routing.module.ts
+++ angularAdvanced/angular-advanced/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'directive-and-pipe',
+  loadChildren: () => import('./lessons/directive-and-pipe/directive-and-pipe.module').then(m => m.DirectiveAndPipeModule)
+}, {
+  path: 'form-advanced',
+  loadChildren: () => import('./lessons/form-advanced/form-advanced.module').then(m => m.FormAdvancedModule)
+}, {
+  path: '', redirectTo: 'directive-and-pipe', pathMatch: 'full'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
