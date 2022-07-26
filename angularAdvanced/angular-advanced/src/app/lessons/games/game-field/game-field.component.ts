3e5db7687b106ae98334b2825f0e64b5fbc042f7
Иванов Юрий Александрович <wolf-04-654@yandex.ru>
2022-07-26 21:43:42 +0300

    рабочая бесконечная матрица (код не оптимизирован) инверсия + пересечине

--- angularAdvanced/angular-advanced/src/app/lessons/games/game-field/game-field.component.ts
+++ angularAdvanced/angular-advanced/src/app/lessons/games/game-field/game-field.component.ts
@@ -12,15 +12,19 @@ import {Location} from '@angular/common';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class GameFieldComponent implements OnInit, AfterViewInit, OnDestroy {
-
+// todo добавить селекты поменять названия полей (добавить реверсивную игру)
   @Select(GameLifeState.deathRule$)
   readonly deathRule$!: Observable<number>;
 
   private deathRule: number = 10;
 
+  private death: number = 4
+
   private destroy$ = new Subject<void>();
 
   public field: number[][] = [];
+
+  private shadowfield: number[][] = [];
 
   constructor(private _location: Location) { }
 
@@ -36,17 +40,30 @@ export class GameFieldComponent implements OnInit, AfterViewInit, OnDestroy {
     this.deathRule$
       .pipe(takeUntil(this.destroy$))
       .subscribe(deathRule => {
-        this.deathRule = deathRule;
+        this.deathRule = Number(deathRule);
         this.createGameField();
       })
   }
 
   createGameField(): void {
     this.field = [];
+    this.shadowfield = [];
     for (let i = 0; i < this.deathRule; i++) {
       this.field.push([]);
+      this.shadowfield.push([]);
       for (let j = 0; j < this.deathRule; j++) {
-        this.field[i].push(j);
+        this.field[i].push(0);
+        this.shadowfield[i].push(0);
+      }
+    }
+  }
+
+  resetShadowfield(): void {
+    this.shadowfield = [];
+    for (let i = 0; i < this.deathRule; i++) {
+      this.shadowfield.push([]);
+      for (let j = 0; j < this.deathRule; j++) {
+        this.shadowfield[i].push(0);
       }
     }
   }
@@ -56,7 +73,59 @@ export class GameFieldComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   start(): void {
-    alert('Запуск игры (не реализован)')
+    for (let row = 0; row < this.deathRule; row++) {
+      for (let cell = 0; cell < this.deathRule; cell++) {
+        // let liveCell = 0;
+
+        if (this.field[row][cell] === 1) {
+
+          this.toggleSell(row - 1, cell - 1);
+          this.toggleSell(row - 1, cell);
+          this.toggleSell(row - 1, cell + 1);
+
+          this.toggleSell(row, cell - 1);
+          this.toggleSell(row, cell, true);
+          this.toggleSell(row, cell + 1);
+
+          this.toggleSell(row + 1, cell - 1);
+          this.toggleSell(row + 1, cell);
+          this.toggleSell(row + 1, cell + 1);
+        }
+
+        //this.death < liveCell ? this.field[row][cell] = 1 : this.field[row][cell] = 0
+      }
+    }
+
+    this.field = Object.assign([], this.shadowfield);
+    this.resetShadowfield();
+    console.log(this.field)
+    console.log(this.shadowfield)
+  }
+
+  getChangeField(changeArrCoordinates: number[][]) {
+    changeArrCoordinates.forEach(cell => {
+      this.field[cell[0]][cell[1]] = cell[2];
+    })
+  }
+
+  toggleSell(row: number, cell: number, reverse: boolean = false): void {
+    if (reverse) return;
+    this.shadowfield[this.infiniteMatrix(row)][this.infiniteMatrix(cell)] ?
+      this.shadowfield[this.infiniteMatrix(row)][this.infiniteMatrix(cell)] = 0 :
+      this.shadowfield[this.infiniteMatrix(row)][this.infiniteMatrix(cell)] = 1
+  }
+
+  infiniteMatrix(index: number): number {
+    if (index === this.deathRule) {
+      return 0;
+    }
+    console.log(index, this.deathRule)
+    return index < 0 ? this.deathRule - 1: index;
+
+  }
+
+  changeCell(i: number, j: number): void {
+    this.field[i][j] ? this.field[i][j] = 0 : this.field[i][j] = 1
   }
 
   ngOnDestroy():void {
