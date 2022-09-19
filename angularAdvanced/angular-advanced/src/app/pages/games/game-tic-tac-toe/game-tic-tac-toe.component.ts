a4b6b401a579e6c3efe6478c8f4ed77d5c47e67c
Иванов Юрий Александрович <wolf-04-654@yandex.ru>
2022-09-19 14:57:54 +0300

    реализовать  лингию больше 3 + компьютера + оповещение победы и блокировку экрана + ресет поля

--- angularAdvanced/angular-advanced/src/app/pages/games/game-tic-tac-toe/game-tic-tac-toe.component.ts
+++ angularAdvanced/angular-advanced/src/app/pages/games/game-tic-tac-toe/game-tic-tac-toe.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 
 export interface PlayersButton {
   players: number;
@@ -8,6 +8,18 @@ export interface PlayersButton {
 export interface FieldOptions {
   name: string;
   value: number;
+}
+
+export interface CheckingWinningCombinations {
+  leftDiagonal: Combinations;
+  verticalLine: Combinations;
+  rightDiagonal: Combinations;
+  horizontalLine: Combinations;
+}
+
+export interface Combinations {
+  isCheck: boolean;
+  line: number[][];
 }
 
 export interface OptionsGame {
@@ -21,12 +33,13 @@ export interface OptionsGame {
 @Component({
   selector: 'game-tic-tac-toe',
   templateUrl: './game-tic-tac-toe.component.html',
-  styleUrls: ['./game-tic-tac-toe.component.sass']
+  styleUrls: ['./game-tic-tac-toe.component.sass'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class GameTicTacToeComponent {
   public toggle: boolean = true;
-  public shadowField: string[][] = []
-  public playersIconList: string[] = ['X', 'O', 'Z', 'V', 'W', 'M', 'Y', '+', '()', '<>', '#', '*']
+  public shadowField: string[][] = [];
+  public playersIconList: string[] = ['X', 'O', 'Z', 'V', 'W', 'M', 'Y', '+', '()', '<>', '#', '*'];
   public readonly playersButtons: PlayersButton[] = [{
     players: 1,
     name: 'С компьютером'
@@ -76,14 +89,14 @@ export class GameTicTacToeComponent {
       this.options.computer = true;
       this.options.players = 2;
     }
-    this.toggle = !this.toggle
+    this.toggle = !this.toggle;
   }
 
   createField(side: number): string[][] {
     let field: string[][] = [];
     for (let i = 0; i < side; i++) {
       field.push([]);
-      for (let k = 0; k < side; k ++) {
+      for (let k = 0; k < side; k++) {
         field[i].push('');
       }
     }
@@ -97,10 +110,10 @@ export class GameTicTacToeComponent {
 
   select(row: number, cell: number): void {
     if (!this.shadowField[row][cell]) {
-      this.shadowField[row][cell] = this.options.icons[this.options.move -1];
+      this.shadowField[row][cell] = this.options.icons[this.options.move - 1];
+      this.checkVictory(row, cell, this.options.move);
+      this.togglePlayer();
     }
-    this.checkVictory(row, cell, this.options.move);
-    this.togglePlayer();
   }
   /**
    * Проверяет если ли совпалдения в символах игрока и переключает из что бы не было повторений
@@ -111,7 +124,7 @@ export class GameTicTacToeComponent {
       [this.options.icons[player], this.options.icons[repeatIndex]] = [icon, this.options.icons[player]];
       return;
     }
-    this.options.icons[player] = icon
+    this.options.icons[player] = icon;
   }
 
   togglePlayer(): void {
@@ -124,8 +137,83 @@ export class GameTicTacToeComponent {
    * */
   checkVictory(row: number, cell: number, move: number) {
     const playerIcon = this.options.icons[move - 1];
+    const winningCombinations: CheckingWinningCombinations = {
+      // \ | / -
+      leftDiagonal: {
+        isCheck: true,
+        line: []
+      },
+      verticalLine: {
+        isCheck: true,
+        line: []
+      },
+      rightDiagonal: {
+        isCheck: true,
+        line: []
+      },
+      horizontalLine: {
+        isCheck: true,
+        line: []
+      },
+    };
 
-    // проверить победу todo РЕАЛИЗОВАТЬ
+    for (let i = 1; i < 3; i++ ) {
+      const leftDiagonal = winningCombinations.leftDiagonal;
+      const verticalLine = winningCombinations.verticalLine;
+      const rightDiagonal = winningCombinations.rightDiagonal;
+      const horizontalLine = winningCombinations.horizontalLine;
+
+      const fieldLength: number = this.shadowField.length;
+      const topEdgeFieldRow: boolean = row - i >= 0;
+      const topEdgeFieldCell: boolean = cell - i >= 0;
+      const bottomEdgeFieldRow: boolean = row + i < fieldLength;
+      const bottomEdgeFieldCell: boolean = cell + i < fieldLength;
+
+      if (leftDiagonal.isCheck) {
+        if ((topEdgeFieldRow && topEdgeFieldCell) && this.shadowField[row - i][cell - i] === playerIcon) leftDiagonal.line.push([row - i, cell - i]);
+        if ((bottomEdgeFieldRow && bottomEdgeFieldCell) && this.shadowField[row + i][cell + i] === playerIcon) leftDiagonal.line.push([row + i, cell + i]);
+        if (leftDiagonal.line.length === 0) leftDiagonal.isCheck = false;
+      }
+
+      if (verticalLine.isCheck) {
+        if (topEdgeFieldCell && this.shadowField[row][cell - i] === playerIcon) verticalLine.line.push([row , cell - i]);
+        if (bottomEdgeFieldCell && this.shadowField[row][cell + i] === playerIcon) verticalLine.line.push([row, cell + i]);
+        if (verticalLine.line.length === 0) verticalLine.isCheck = false;
+      }
+
+      if (rightDiagonal.isCheck) {
+        if ((bottomEdgeFieldRow && topEdgeFieldCell ) && this.shadowField[row + i][cell - i] === playerIcon) rightDiagonal.line.push([row + i, cell - i]);
+        if ((topEdgeFieldRow && bottomEdgeFieldCell) && this.shadowField[row - i][cell + i] === playerIcon) rightDiagonal.line.push([row - i, cell + i]);
+        if (rightDiagonal.line.length === 0) rightDiagonal.isCheck = false;
+      }
+
+      if (horizontalLine.isCheck) {
+        if (topEdgeFieldRow && this.shadowField[row - i][cell] === playerIcon) horizontalLine.line.push([row - i, cell]);
+        if (bottomEdgeFieldRow && this.shadowField[row + i][cell] === playerIcon) horizontalLine.line.push([row + i, cell]);
+        if (horizontalLine.line.length === 0) horizontalLine.isCheck = false;
+      }
+    }
+// todo Сделать лингию больше 3 + компьютера + оповещение победы и блокировку экрана + ресет поля
+    if (winningCombinations.leftDiagonal.line.length === 2) {
+      console.log("win leftDiagonal")
+      alert('Выиграл ' + playerIcon)
+    }
+
+    if (winningCombinations.verticalLine.line.length === 2) {
+      console.log("win verticalLine")
+      alert('Выиграл ' + playerIcon)
+    }
+
+    if (winningCombinations.rightDiagonal.line.length === 2) {
+      console.log("win rightDiagonal")
+      alert('Выиграл ' + playerIcon)
+    }
+
+    if (winningCombinations.horizontalLine.line.length === 2) {
+      console.log("win horizontalLine")
+      alert('Выиграл ' + playerIcon)
+    }
+
   }
 
 
