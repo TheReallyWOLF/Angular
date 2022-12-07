f0b3889ae522db8babeea803cfe8d03da8d9562c
Иванов Юрий Александрович <wolf-04-654@yandex.ru>
2022-12-07 19:59:52 +0300

    drag and drop api v 1.2 release

--- angularAdvanced/angular-advanced/src/app/pages/css-layout-examples/drag-and-drop-api/drag-and-drop-api.component.ts
+++ angularAdvanced/angular-advanced/src/app/pages/css-layout-examples/drag-and-drop-api/drag-and-drop-api.component.ts
@@ -13,12 +13,17 @@ import {
 export class DragAndDropApiComponent {
   private currentEl: HTMLElement | undefined;
 
+  private parentElement: HTMLElement | undefined
+
   readonly dropZoneArr: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
   constructor(private renderer: Renderer2) {}
 
   dragDrop(event: DragEvent): void {
     event.preventDefault();
+
+    this.updateParentElement(event.target as HTMLElement)
+
     if (this.currentEl && this.currentEl !== event.target) {
       (event.target as HTMLElement).append(this.currentEl);
     }
@@ -26,15 +31,26 @@ export class DragAndDropApiComponent {
 
   dragLeave(event: DragEvent): void {
     event.preventDefault();
+
     this.renderer.removeClass(event.target, 'drop-zone-over');
   }
 
   dragOver(event: DragEvent): void {
     event.preventDefault();
+
     this.renderer.addClass(event.target, 'drop-zone-over');
   }
 
   dragStart(event: DragEvent): void {
     this.currentEl = event.target as HTMLElement;
+  }
+
+  updateParentElement (element: HTMLElement): void {
+    if (this.parentElement) {
+      this.renderer.removeClass(this.parentElement, 'active-zone');
+    }
+
+    this.parentElement = element;
+    this.renderer.addClass(this.parentElement, 'active-zone');
   }
 }
