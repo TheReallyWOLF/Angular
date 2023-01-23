7f56be538957f7e25947f9313f2deffc7b3981bd
Иванов Юрий Александрович <wolf-04-654@yandex.ru>
2023-01-23 16:59:15 +0300

    лендинг по массажу v 0.011

--- massageLandingPage/src/app/app-routing.module.ts
+++ massageLandingPage/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home-page',
+    pathMatch: 'full'
+  }, {
+    path: 'home-page',
+    loadChildren: () => import('./pages/home-page/home-page.module').then(m => m.HomePageModule)
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- massageLandingPage/src/app/app.module.ts
+++ massageLandingPage/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common-components/header/header.component';
+import { FooterComponent } from './common-components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
