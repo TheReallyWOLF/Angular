88902427c6169397a4376957b6ba5f0e512cf1d7
Иванов Юрий Александрович <wolf-04-654@yandex.ru>
2023-01-26 14:30:55 +0300

    лендинг по массажу v 0.52

---
+++ massageLandingPage/src/app/components/indications-massage/indications-massage.component.ts
@@ -0,0 +1,9 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+
+@Component({
+  selector: 'indications-massage',
+  templateUrl: './indications-massage.component.html',
+  styleUrls: ['./indications-massage.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class IndicationsMassageComponent {}
---
+++ massageLandingPage/src/app/components/massage-contraindications/massage-contraindications.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'contraindications-massage',
+  templateUrl: './massage-contraindications.component.html',
+  styleUrls: ['./massage-contraindications.component.scss']
+})
+export class MassageContraindicationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
