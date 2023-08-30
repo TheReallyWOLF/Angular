978a7c95182da7bce159cdbc2e1d1533b3d41215
Иванов Юрий Александрович <wolf-04-654@yandex.ru>
2023-08-30 10:36:20 +0300

    npm i

--- angularAdvanced/angular-advanced/src/app/shared/directive/atributeDirective/host-binding.directive.ts
+++ angularAdvanced/angular-advanced/src/app/shared/directive/atributeDirective/host-binding.directive.ts
@@ -1,7 +1,7 @@
 import {Directive, HostBinding, HostListener} from '@angular/core';
 
 @Directive({
-  selector: '[appHostBinding]'
+  selector: '[appHostBinding]',
 })
 export class HostBindingDirective {
 
